42d68fb51a76ba7d063ac79c36e92bc44379d4aa
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-09-02 22:26:44 +0530

    Initial Commit

---
+++ MVVMV_SwiftUI/MVVMV_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMV_SwiftUIApp.swift
+//  MVVMV_SwiftUI
+//
+//  Created by Pranav Choudhary on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMV_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
